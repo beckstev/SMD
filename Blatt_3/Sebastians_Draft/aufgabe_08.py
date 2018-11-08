218a6197824511dafaccc75e5f0325aa80df3bcc
Sebastian-Pape <SePa@gmx.de>
2018-11-08 17:33:51 +0100

    draft blatt 3

---
+++ Blatt_3/Sebastians_Draft/aufgabe_08.py
@@ -0,0 +1,106 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import brentq
+
+np.random.seed(0)
+
+# möglicher Algorithmus für das Neumannsche Rückweisungsverfahren
+def Rückweisungsverfahren(function, Anzahl_zufallszahlen, x, y):
+
+    verteilung_x = []
+    verteilung_y = []
+
+    for i in range(Anzahl_zufallszahlen):
+        if y[i] <= function(x[i]):
+            verteilung_x.append(x[i])
+            verteilung_y.append(y[i])
+    return verteilung_x, verteilung_y            
+
+# Normierungskonstante der Planck-Verteilung
+N = np.pi**4 / 15
+
+def planck_verteilung(x):
+    return N * x**3 / (np.exp(x) - 1)
+
+################## Aufgabenteil a.) ##################
+
+# Anzahl der Zufallszahlen
+Anzahl_zufallszahlen = int(4.39994e5)
+
+# obere Grenze der x-Werte
+x_cut = 20
+
+# untere Grenze der x-Werte
+x_min = 0
+
+# x Intervall
+x_intervall = np.linspace(x_min, x_cut, Anzahl_zufallszahlen)
+
+#obere Grenze der y-Werte
+def ableitung_planck(x):
+    return (3 * x**2 * (np.exp(x) - 1) - x**3 * np.exp(x)) / (np.exp(x) - 1)**2
+
+y_cut = planck_verteilung(brentq(ableitung_planck, x_min + 0.5, x_cut))
+
+#untere Grenze der y-Werte
+y_min = planck_verteilung(x_cut)
+
+# Zufallszahlen der y-Achse der Planck Verteilung 
+random_y = np.random.uniform(y_min, y_cut, Anzahl_zufallszahlen)
+
+# Zufallszahlen der x-Achse der gegebenen Verteilungsfunktion
+random_x = np.sort(np.random.uniform(x_min, x_cut, Anzahl_zufallszahlen))
+
+verteilung_x, verteilung_y = Rückweisungsverfahren(planck_verteilung, Anzahl_zufallszahlen, random_x, random_y)
+
+# Plotten der durch das Neumannsche Rückweisungsverfahren gefilterten Zufallszahlen
+plt.plot(verteilung_x, verteilung_y, 'r.', label = 'Planckverteilte Zufallszahlen')
+# Plotten der Daten der Planck Verteilung zum Vergleichen
+plt.plot(x_intervall, planck_verteilung(x_intervall), 'b-', label = 'Planckverteilung')
+plt.legend(loc = 'best')
+plt.tight_layout()
+plt.savefig('Pics/planck_verteilung.pdf')
+
+print(len(verteilung_x))
+
+################## Aufgabenteil b.) ##################
+
+# Bestimmen von x_s
+
+def majorante_xs(x):
+    return y_cut - 200 * N * x**(-0.1) * np.exp(-x**(0.9))   
+
+x_s = brentq(majorante_xs, 0.1, x_cut)
+
+# Inversionsmethode
+
+N_trafo = 9 / 2000 / (np.exp(-x_s**(0.9)) - np.exp(-x_cut**(0.9)))
+
+def transformation_rückweisung(function, Anzahl, x_s, x_min, x_max, y_min, y_max):
+    buffer_x = np.sort(np.random.uniform(x_min, x_max, Anzahl))
+    buffer_y = np.random.uniform(y_min, y_max, Anzahl)
+    index = len(buffer_x)
+    for n in range(len(buffer_x)):
+        if buffer_x[n] > x_s:
+            index  = n
+            break
+    buffer_0to1 = np.random.uniform(0, 1, len(buffer_x[index:]))        
+    buffer_x[index:] = transformation(buffer_0to1)
+    buffer_y[index:] = majorante_planck(transformation(buffer_0to1)) * np.flip(buffer_0to1)
+    return Rückweisungsverfahren(function, Anzahl, buffer_x, buffer_y)
+
+def transformation(u):
+    return (-np.log(-9 * u / (2000 * N_trafo) + np.exp(-x_s**(0.9))))**(10 / 9)
+
+def majorante_planck(x):
+    return 200 * N * x**(-0.1) * np.exp(-x**(0.9))
+
+# Erzeugen der Zufallszahlen gemäß der Majorantenfnktion
+planck_majorante_x, planck_majorante_y = transformation_rückweisung(planck_verteilung, Anzahl_zufallszahlen, x_s, x_min, x_cut, y_min, y_cut)
+
+plt.clf()
+plt.plot(planck_majorante_x, planck_majorante_y, 'r.', label = 'Planckverteilte Zufallszahlen')
+plt.plot(x_intervall, planck_verteilung(x_intervall), 'b-', label = 'Planckverteilung')
+plt.legend(loc = 'best')
+plt.tight_layout()
+plt.savefig('Pics/Zufallszahlen_aus_Majorante_planck.pdf')
---
+++ Blatt_3/Sebastians_Draft/aufgabe_09.py
@@ -0,0 +1,55 @@
+import numpy as np#
+import matplotlib.pyplot as plt
+
+np.random.seed(42)
+
+def planck_verteilung(x):
+    return 15 / np.pi**4 * x**3 / (np.exp(x) - 1)
+
+def Übergangswahrsch(function, x_i, x_j):
+    return min(1, function(x_j) / function(x_i))
+
+def Vorschlags_PDF(x, s):
+    return abs(np.random.uniform(x - s, x + s, 200)[np.random.random_integers(0, 199, 1)])
+
+def Metropolis(function, x_0, s, anzahl):
+    x = [x_0]
+    random = np.random.uniform(0, 1, anzahl)
+    for i in range(anzahl - 1):
+        x_j = Vorschlags_PDF(x[i], s)
+        if random[i] <= Übergangswahrsch(function, x[i], x_j):
+            x.append(x_j)
+        else:
+            x.append(x[i])           
+    return x   
+
+# Startwert
+x_0 = 30
+
+# Schrittweite
+step_size = 2
+
+anzahl = int(1e5)
+
+zufallszahlen_planck = Metropolis(planck_verteilung, x_0, step_size, anzahl)
+
+plt.clf()
+plt.hist(zufallszahlen_planck, bins = 100, density=True, histtype='step', label='Zufallszahlen aus Metropolis')
+plt.plot(np.linspace(0.01, 30, 1000), planck_verteilung(np.linspace(0.01, 30, 1000)), label='Planck-Verteilung')
+plt.ylabel('y')
+plt.xlabel('x')
+plt.legend(loc='best')
+plt.savefig('Pics/a9_b_c.pdf')
+
+plt.clf()
+plt.plot(np.linspace(1, anzahl, anzahl), zufallszahlen_planck, 'g,', label='Iteratrionsschritt gegen Zufallszahl')
+plt.xscale('log')
+plt.ylabel('y')
+plt.xlabel('x')
+plt.legend(loc='best')
+plt.savefig('Pics/a9_d_Trace_plot.pdf')
+
+print(planck_verteilung(np.mean(zufallszahlen_planck)), max(planck_verteilung(np.linspace(0.01, 30, 1000))))
+
+# Die erzeugten Zufallszahlen konvergieren in den ersten hundert Iterationen sehr schnell gegen kleine x-Werte und fluktuieren dann
+# anschließend in der Umgebung um das Maximum umher
